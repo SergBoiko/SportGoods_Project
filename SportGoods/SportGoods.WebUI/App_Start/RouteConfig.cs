c4cfb9a9e9d24039b0782d58a53f9f7ff8298cf4
Serhii <sboyko1995@gmail.com>
2020-01-13 13:40:51 +0200

    added db and products table

--- SportGoods/SportGoods.WebUI/App_Start/RouteConfig.cs
+++ SportGoods/SportGoods.WebUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SportGoods.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "SportProduct", action = "List", id = UrlParameter.Optional }
             );
         }
     }
