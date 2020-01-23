90621251a46b90515184b0c16bdbc9ec957991d9
Serhii <sboyko1995@gmail.com>
2020-01-23 15:06:24 +0200

    added admin controller and started doing admin page

---
+++ SportGoods/SportGoods.UnitTests/AdminTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportGoods.Domain.Abstract;
+using SportGoods.Domain.Entities;
+using SportGoods.WebUI.Controllers;
+
+namespace SportGoods.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Games()
+        {
+            // Организация - создание имитированного хранилища данных
+            Mock<ISportProductRepository> mock = new Mock<ISportProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product { Id = 1, Name = "Product1"},
+                new Product { Id = 2, Name = "Product2"},
+                new Product { Id = 3, Name = "Product3"},
+                new Product { Id = 4, Name = "Product4"},
+                new Product { Id = 5, Name = "Product5"}
+            });
+
+            // Организация - создание контроллера
+            AdminController controller = new AdminController(mock.Object);
+
+            // Действие
+            List<Product> result = ((IEnumerable<Product>)controller.Index().
+                ViewData.Model).ToList();
+
+            // Утверждение
+            Assert.AreEqual(result.Count(), 5);
+            Assert.AreEqual("Product1", result[0].Name);
+            Assert.AreEqual("Product2", result[1].Name);
+            Assert.AreEqual("Product3", result[2].Name);
+        }
+
+    }
+}
---
+++ SportGoods/SportGoods.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using SportGoods.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportGoods.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        ISportProductRepository repository;
+        
+        public AdminController(ISportProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
